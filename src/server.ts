498f462a674eb01ba40487d2bbc773838f889b84
Ener <renep2010@gmail.com>
2025-09-08 03:27:19 -0300

    updating port to 3002

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Start server
 app.listen(PORT, () => {
